f1b2af4afc1a211b52476f7b511d0ffa41f4a0a3
BumpVersion Action <bumpversion@github-actions>
2023-03-09 11:44:18 +0000

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="pv_forecast",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     description="ML experiments to forecast national solar PV power in GB.",
     author="Jack Kelly, Jacob Bieker, Peter Dudfield",
